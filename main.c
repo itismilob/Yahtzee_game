f03b0aeea2ea18e9dc32a148b6acb3fd311b0783
itismilob <itismilob@gmail.com>
2024-12-06 11:13:36 +0900

    fix

--- main.c
+++ main.c
@@ -12,30 +12,30 @@ void gameRound();
 
 void lock();
 
-void combi();
+int combi();
 
 void roll();
 
 
 // 유저 입력
-void choice();
+void getInput();
 
 // 출력
 void scoreboard();
 
 void rules();
 
-// 메인
-int main();
 
 //주사위관련
+// DiceData로 변경
 int dice[5] = {0};
+
 int locking[5] = {0};
 int numbers[6] = {0};
 int remain = 0;
 
 // 유저 입력
-char says[100];
+char user_input[100];
 
 //판정 관련
 int combi_score = 0;
@@ -44,9 +44,12 @@ char *combiList[13] = {
         "One", "Two", "Three", "Four", "Five", "Six", "Three of Kinds", "Four of Kinds", "Full House", "Small Straight",
         "Large Straight", "Yahtzee", "Chance"
 };
+
+// DiceData로 변경
 int isStop;
 
 //점수 관련
+// score는 ScoreData로 변경
 int score[13] = {0};
 int checking[13] = {0};
 int total = 0;
@@ -56,6 +59,8 @@ int i;
 
 
 //라운드가 시작할때 작동됩니다.
+// 내턴이면 라운드 진행
+// 턴이 아니면 recv로 대기
 void gameRound() {
     int R = 0;
     while (R < 13) {
@@ -69,16 +74,15 @@ void gameRound() {
         remain = 3;
 
         printf("주사위를 굴리기 위해 아무 글자나 입력하세요\n");
-        choice();
+        getInput();
 
         roll();
-
-        isStop = 0;
         lock();
     }
 
     // GAME END
     scoreboard();
+
     printf("최종 점수 : %d", total);
 }
 
@@ -90,13 +94,20 @@ void roll() {
             dice[i] = rand() % 6 + 1;
         }
     }
-    // 여기서 서버로 전송함
+
+    // Yahtzee 테스트용
+//    for (i = 0; i < 5; i++) {
+//        dice[i] = 6;
+//    }
+
+    // 여기서 서버로 전송함 #2-1
 
     remain = remain - 1;
 }
 
 //락을 시킬 때 발동됩니다.
 void lock() {
+    isStop = 0;
     while (1) {
         scoreboard();
 
@@ -109,39 +120,6 @@ void lock() {
             }
         }
 
-        printf("\n남은 Roll 수 : %d\n", remain);
-
-        if (remain > 0 && isStop == 0) {
-            printf("락을 시키려거나 해제하려면 주사위 자리번호를 쓰세요.\n");
-            printf("주사위 자리번호는 왼쪽부터 1번입니다.\n");
-            printf("주사위를 다시 굴리고 싶다면 \"roll\"을 치세요\n");
-            printf("조합을 채우고 라운드를 끝내고 싶다면 \"stop\"을 쳐서 조합 선택 메뉴로 들어가세요.\n");
-            choice();
-            if (!strcmp(says, "1") || !strcmp(says, "2") || !strcmp(says, "3") || !strcmp(says, "4") || !
-                    strcmp(says, "5")) {
-                // lock을 변경함 1->0 0->1
-                int temp = atoi(says) - 1;
-                locking[temp] = locking[temp] == 1 ? 0 : 1;
-            } else if (strcmp(says, "roll") == 0) {
-                roll();
-            } else if (strcmp(says, "stop") == 0) {
-                isStop = 1;
-            } else {
-                printf("잘못 입력하셨습니다.\n");
-            }
-        } else {
-            combi();
-            return;
-        }
-    }
-}
-
-
-//조합에 따라 움직입니다.
-void combi() {
-    while (1) {
-        printf("\n조합의 번호를 입력하세요.\n");
-        printf("돌아가려면 back을 입력하세요\n");
 
         // 숫자 배열 초기화
         for (i = 0; i < 6; i++) {
@@ -150,50 +128,93 @@ void combi() {
         // 주사위로 숫자 배열 입력
         for (i = 0; i < 5; i++) {
             numbers[dice[i] - 1]++;
-        }
-        combi_score = 0;
 
-        if ((numbers[0] == 5 || numbers[1] == 5 || numbers[2] == 5 || numbers[3] == 5 || numbers[4] == 5 || numbers[5]
-                                                                                                            ==
-                                                                                                            5) && score[11]) {
-            if (score[11] != 50) {
-                // 첫 번째 얏찌 확인
-                printf("\nYahtzee!\n");
+        }
+
+        if (numbers[0] == 5 || numbers[1] == 5 || numbers[2] == 5 || numbers[3] == 5 || numbers[4] == 5 ||
+            numbers[5] == 5) {
+            // 첫 번째 얏찌 확인
+            printf("\nYahtzee!\n");
+        }
+
+        printf("\n남은 Roll 수 : %d\n", remain);
+
+        if (remain > 0 && isStop == 0) {
+            printf("락을 시키려거나 해제하려면 주사위 자리번호를 쓰세요.\n");
+            printf("주사위 자리번호는 왼쪽부터 1번입니다.\n");
+            printf("주사위를 다시 굴리고 싶다면 \"roll\"을 치세요\n");
+            printf("조합을 채우고 라운드를 끝내고 싶다면 \"stop\"을 쳐서 조합 선택 메뉴로 들어가세요.\n");
+            getInput();
+            if (!strcmp(user_input, "1") || !strcmp(user_input, "2") || !strcmp(user_input, "3") ||
+                !strcmp(user_input, "4") || !
+                        strcmp(user_input, "5")) {
+                // lock을 변경함 1->0 0->1
+                int temp = atoi(user_input) - 1;
+                locking[temp] = locking[temp] == 1 ? 0 : 1;
+            } else if (strcmp(user_input, "roll") == 0) {
+                roll();
+            } else if (strcmp(user_input, "stop") == 0) {
+                isStop = 1;
             } else {
-                // 두번째 얏찌 확인
-                printf("\nback-to-back Yahtzee bonus +50\n");
-                yahtzee_bonus = 50;
-                combi_score += 50;
+                printf("잘못 입력하셨습니다.\n");
             }
-        }
+        } else { ;
+            // 조합 선택이 완료된건지 back을 선택한건지 확인함
+            if (combi() == 1) {
+                // 조합 선이 끝나고 턴이 끝나면 다음 유저로 넘어감
+                
 
-        int selected = -1;
-        // 입력 받는 부분 반복임
-        while (1) {
-            choice();
-            if (strcmp(says, "back") == 0) {
                 return;
             }
+        }
+    }
+}
 
-            char *selectedList[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
-            for (i = 0; i < 13; i++) {
-                if (strcmp(says, selectedList[i]) == 0) {
-                    selected = i;
-                    break;
-                }
-            }
 
-            if (selected == -1) {
-                printf("잘못 입력하셨거나 이미 채워진 조합입니다.\n");
-            } else {
+//조합에 따라 움직입니다.
+int combi() {
+    while (1) {
+        combi_score = 0;
+
+        if ((numbers[0] == 5 || numbers[1] == 5 || numbers[2] == 5 || numbers[3] == 5 || numbers[4] == 5 ||
+             numbers[5] == 5) && score[11] == 50) {
+            // 두번째 얏찌 확인
+            printf("\nback-to-back Yahtzee bonus +50\n");
+            yahtzee_bonus = 50;
+            combi_score += 50;
+        }
+
+        printf("\n조합의 번호를 입력하세요.\n");
+        printf("돌아가려면 back을 입력하세요\n");
+
+
+        int selected = -1;
+        getInput();
+
+        if (strcmp(user_input, "back") == 0) {
+            // stop을 취소하고 돌아감
+            isStop = 0;
+            return 0;
+        }
+
+        char *selectedList[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+        for (i = 0; i < 13; i++) {
+            if (strcmp(user_input, selectedList[i]) == 0) {
+                selected = i;
                 break;
             }
         }
 
+        if (selected == -1) {
+            printf("잘못 입력하셨거나 이미 채워진 조합입니다.\n");
+            continue;
+        }
+
+
         // One ~ Six
         for (i = 0; i < 6; i++) {
             if (selected == i && checking[i] == 0) {
-                combi_score = numbers[i] * (i + 1);
+                combi_score += numbers[i] * (i + 1);
             }
         }
 
@@ -232,7 +253,7 @@ void combi() {
             }
 
             if (dou == 1 && tri == 1) {
-                combi_score = 25;
+                combi_score += 25;
             }
         }
 
@@ -241,21 +262,21 @@ void combi() {
             if ((numbers[0] >= 1 && numbers[1] >= 1 && numbers[2] >= 1 && numbers[3] >= 1) || (
                     numbers[1] >= 1 && numbers[2] >= 1 && numbers[3] >= 1 && numbers[4] >= 1) || (
                         numbers[2] >= 1 && numbers[3] >= 1 && numbers[4] >= 1 && numbers[5] >= 1)) {
-                combi_score = 30;
+                combi_score += 30;
             }
         }
         // Large straight
         if (selected == 10 && checking[10] == 0) {
             if ((numbers[0] == 1 && numbers[1] == 1 && numbers[2] == 1 && numbers[3] == 1 && numbers[4] == 1) || (
                     numbers[1] == 1 && numbers[2] == 1 && numbers[3] == 1 && numbers[4] == 1 && numbers[5] == 1)) {
-                combi_score = 40;
+                combi_score += 40;
             }
         }
         // Yahtzee
         if (selected == 11 && checking[11] == 0) {
             for (i = 0; i < 6; i++) {
                 if (numbers[i] == 5) {
-                    combi_score = 50;
+                    combi_score += 50;
                 }
             }
         }
@@ -269,14 +290,16 @@ void combi() {
 
         while (1) {
             printf("%s 의 점수는 %d입니다. 최종 결정은 yes, 다시 돌아가려면 no\n", combiList[selected], combi_score);
-            choice();
+            getInput();
 
-            if (strcmp(says, "yes") == 0) {
+            if (strcmp(user_input, "yes") == 0) {
                 score[selected] = combi_score;
                 checking[selected] = 1;
-                return;
-            }
-            if (strcmp(says, "no") != 0) {
+                scoreboard();
+                return 1;
+            } else if (strcmp(user_input, "no") == 0) {
+                break;
+            } else {
                 printf("잘못 입력했습니다.\n");
             }
         }
@@ -304,13 +327,16 @@ void scoreboard() {
         total = total + score[i];
     }
     total = total + yahtzee_bonus;
-    for (i = 1; i <= 6; i++) {
+    for (i = 0; i < 6; i++) {
         bonus = bonus + score[i];
     }
     if (bonus >= 63) {
         total = total + 35;
     }
 
+    for (i = 0; i < 100; i++) {
+        printf("\n");
+    }
     printf("-Yahtzee--------------- user1 | user2 -\n");
     printf("[1] One                   %-3s | %3s\n", formatScore(0), formatScore(0));
     printf("[2] Two                   %-3s | %3s\n", formatScore(1), formatScore(1));
@@ -319,7 +345,7 @@ void scoreboard() {
     printf("[5] Five                  %-3s | %3s\n", formatScore(4), formatScore(4));
     printf("[6] Six                   %-3s | %3s\n", formatScore(5), formatScore(5));
     printf("bonus 63 /                %-3d | %3d\n", bonus, bonus);
-    printf("--------------------------------------\n");
+    printf("---------------------------------------\n");
     printf("[7] Three of Kinds        %-3s | %3s\n", formatScore(6), formatScore(6));
     printf("[8] Four of Kinds         %-3s | %3s\n", formatScore(7), formatScore(7));
     printf("[9] Full House            %-3s | %3s\n", formatScore(8), formatScore(8));
@@ -327,10 +353,10 @@ void scoreboard() {
     printf("[11] Large Straight       %-3s | %3s\n", formatScore(10), formatScore(10));
     printf("[12] Yahtzee              %-3s | %3s\n", formatScore(11), formatScore(11));
     printf("[13] Chance               %-3s | %3s\n", formatScore(12), formatScore(12));
-    printf("--------------------------------------\n");
+    printf("---------------------------------------\n");
     printf("Yahtzee bonus             %-3d | %3d\n", yahtzee_bonus, yahtzee_bonus);
     printf("Total score               %-3d | %3d\n", total, total);
-    printf("--------------------------------------\n");
+    printf("---------------------------------------\n");
 }
 
 //룰 설명입니다.
@@ -343,8 +369,8 @@ void rules() {
     printf("한번 점수를 얻은 조합 종류로는 다시 점수를 획득할 수 없습니다.\n\n");
     printf("조합 종류를 보시려면 \"next\"를 써주세요.\n");
     printf("메인 메뉴로 돌아가시려면 아무 글자나 써주세요.\n");
-    choice();
-    if (strcmp(says, "next") == 0) {
+    getInput();
+    if (strcmp(user_input, "next") == 0) {
         printf("\n\n");
         printf("------Yahtzee------\n");
         printf("One : 주사위에서 1이 나온 주사위 개수 x1을 점수로 합니다.\n");
@@ -373,9 +399,9 @@ void rules() {
 }
 
 // 유저 입력 받는거임
-void choice() {
-    memset(says, 0, 100);
-    scanf("%s", &says);
+void getInput() {
+    memset(user_input, 0, 100);
+    scanf("%s", user_input);
 }
 
 //처음 시작할때 발동되는 것들 입니다.
@@ -385,10 +411,13 @@ int main() {
         printf("룰을 듣고싶다면 \"rule\"을\n");
         printf("게임을 시작하려면 \"start\"를 써주세요.\n");
 
-        choice();
-        if (strcmp(says, "rule") == 0) {
+        getInput();
+        if (strcmp(user_input, "rule") == 0) {
             rules();
-        } else if (strcmp(says, "start") == 0) {
+        } else if (strcmp(user_input, "start") == 0) {
+            // 여기서 서버에 접속함 #1-2
+
+            // 이건 게임을 시작함
             gameRound();
             break;
         } else {
